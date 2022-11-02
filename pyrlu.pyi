08d8c44d3ee2be02b153e9b6f21ab515d77adb41
Richard Lincoln <r.w.lincoln@gmail.com>
2022-11-02 11:51:12 +0100

    Adding stub file with type information for MyPy.

---
+++ pyrlu.pyi
@@ -0,0 +1,6 @@
+from numpy import int32, float64, complex128
+from numpy.typing import NDArray
+
+def factor_solve(n: int, rowind: NDArray[int32], colptr: NDArray[int32], nz: NDArray[float64], b: NDArray[float64], trans: bool): ...
+
+def z_factor_solve(n: int, rowind: NDArray[int32], colptr: NDArray[int32], nz: NDArray[complex128], b: NDArray[complex128], trans: bool): ...
